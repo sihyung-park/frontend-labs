9274228712c5ea4cc40d004fb0bf69129e2d344b
sihyung-park <sihyung.park@modulabs.co.kr>
2025-03-21 12:53:39 +0900

    feat : jupyter colab test

---
+++ apps/web/app/test/page.tsx
@@ -0,0 +1,18 @@
+export default function Test() {
+  return (
+    <div>
+      <a href="https://colab.research.google.com/github/escape1001/ormcamp/blob/main/python/240116_lms_%ED%81%AC%EB%A1%A4%EB%A7%81%EC%97%B0%EC%8A%B5%EB%AC%B8%EC%A0%9C.ipynb">
+        <img
+          src="https://colab.research.google.com/assets/colab-badge.svg"
+          alt="Open In Colab"
+        />
+      </a>
+      <a href="https://colab.research.google.com/github/sihyung-park/frontend-labs/blob/main/colab_test.ipynb">
+        <img
+          src="https://colab.research.google.com/assets/colab-badge.svg"
+          alt="Open In Colab"
+        />
+      </a>
+    </div>
+  );
+}
