f437bcd9c02c7a61d37dcaf94c48a1360af511ec
sihyung-park <sihyung.park@modulabs.co.kr>
2025-06-05 18:05:04 +0900

    feat : trigger 테스트

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -13,7 +13,6 @@ const ThemeImage = (props: Props) => {
   return (
     <>
       <Image {...rest} src={srcLight} className="imgLight" />
-      <Image {...rest} src={srcDark} className="imgDark" />
     </>
   );
 };
